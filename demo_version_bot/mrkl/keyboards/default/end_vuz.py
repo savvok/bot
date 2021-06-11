7a22697aaef7ce01f363f2f99026fe92080002e4
Masha Markelova <a-anufri@yandex.ru>
2021-06-11 13:27:56 +0000

    Add new file

---
+++ demo_version_bot/mrkl/keyboards/default/end_vuz.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+endvuz = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton("В начало"),
+            KeyboardButton("Пока хватит)")
+        ]
+    ])
